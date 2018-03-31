550a20ec1a39bc0bb6916f258de77dcdcf05ed3a
rsoni123 <Amaya123>
2018-03-31 19:42:19 -0400

    Initial commit

---
+++ app/src/main/java/whizkid/amaya/calculatorforlaunch/Notes.java
@@ -0,0 +1,10 @@
+package whizkid.amaya.calculatorforlaunch;
+
+public class Notes {
+//    Coloured buttons
+//    https://stackoverflow.com/questions/26346727/android-material-design-button-styles
+
+//    https://androidappsforme.com/calculator-apps/
+
+
+}
---
+++ app/src/main/java/whizkid/amaya/calculatorforlaunch/Utils.java
@@ -0,0 +1,89 @@
+package whizkid.amaya.calculatorforlaunch;
+
+public class Utils {
+
+    public static final String ADD = "+";
+    public static final String SUBTRACT = "-";
+    public static final String MULTIPLY = "*";
+    public static final String DIVIDE = "/";
+    public static final String ALL_CLEAR = "ALLCLEAR";
+    public static final String EVALUATE = "=";
+
+    public static boolean isTagOperator(String tag) {
+        return ADD.equals(tag) || SUBTRACT.equals(tag)
+                || MULTIPLY.equals(tag) || DIVIDE.equals(tag);
+    }
+
+    public static boolean isTagEvaluateOperator(String tag) {
+        return EVALUATE.equals(tag);
+    }
+
+    public static boolean isItClearTagOperator(String tag) {
+        return ALL_CLEAR.equals(tag);
+    }
+
+
+    public static String[] splitMyString(String problem) {
+        String[] tokens = problem.split("\\+|\\-|\\*|\\/");
+
+        return tokens;
+    }
+
+
+    public static boolean containsAnyTagAlready(String str, String searchChars) {
+        if (searchChars == null) {
+            return false;
+        }
+        return containsAny(str, searchChars.toCharArray());
+    }
+
+    public static boolean containsAny(String str, char[] searchChars) {
+        if (str == null || str.length() == 0 || searchChars == null || searchChars.length == 0) {
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            for (int j = 0; j < searchChars.length; j++) {
+                if (searchChars[j] == ch) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Computes the file
+     * @param equation
+     * @return
+     */
+    static String evalMe(String equation) {
+        System.out.println("equation " + equation);
+
+        double result = 0.0;
+        String noMinus = equation.replace("-", "+-");
+        String[] byPluses = noMinus.split("\\+");
+
+        for (String multipl : byPluses) {
+            String[] byMultipl = multipl.split("\\*");
+            double multiplResult = 1.0;
+            for (String operand : byMultipl) {
+                if (operand.contains("/")) {
+                    String[] division = operand.split("\\/");
+                    double divident = Double.parseDouble(division[0]);
+                    for (int i = 1; i < division.length; i++) {
+                        divident /= Double.parseDouble(division[i]);
+                    }
+                    multiplResult *= divident;
+                } else {
+                    multiplResult *= Double.parseDouble(operand);
+                }
+            }
+            result += multiplResult;
+        }
+
+        System.out.println("result " + result);
+        return Double.toString(result);
+    }
+
+}
