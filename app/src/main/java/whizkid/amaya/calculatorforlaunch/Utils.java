759a2a82eb95582a16f2d24750f7b3498cc53724
rsoni123 <Amaya123>
2018-04-05 15:28:59 -0400

    cleanup started for equation

--- app/src/main/java/whizkid/amaya/calculatorforlaunch/Utils.java
+++ app/src/main/java/whizkid/amaya/calculatorforlaunch/Utils.java
@@ -105,6 +105,7 @@ public class Utils {
         equation = equation.replace("++", "+");
         equation = equation.replace("---", "+-");
         equation = equation.replace("--", "+");
+        equation = equation.replace("+-", "-");
 
         equation = equation.replace("..", ".");
 
