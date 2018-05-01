9c66b23b3d4ce648e0ad09da5dfe8a203b048fd2
rsoni123 <Amaya123>
2018-04-30 21:10:24 -0400

    Non solid Color Operator Button

--- app/src/main/java/whizkid/amaya/calculatorforlaunch/CalculatorCustomAdapterRecycler.java
+++ app/src/main/java/whizkid/amaya/calculatorforlaunch/CalculatorCustomAdapterRecycler.java
@@ -3,6 +3,7 @@ package whizkid.amaya.calculatorforlaunch;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -17,18 +18,25 @@ public class CalculatorCustomAdapterRecycler extends
     private LayoutInflater mInflater;
     private ArrayList<HistoryTasks> mDataSource;
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        public TextView mTextView;
 
-        public ViewHolder(TextView v) {
-            super(v);
-            mTextView = v;
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        TextView textEquation;
+        TextView textResult;
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(View itemView) {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+
+            textEquation = (TextView) itemView.findViewById(R.id.history_list_equation);
+            textResult = (TextView) itemView.findViewById(R.id.history_list_result);
         }
-
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
@@ -41,21 +49,29 @@ public class CalculatorCustomAdapterRecycler extends
     @Override
     public CalculatorCustomAdapterRecycler.ViewHolder onCreateViewHolder(ViewGroup parent,
                                                    int viewType) {
-        // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.row_item, parent, false);
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
 
-        ViewHolder vh = new ViewHolder(v);
-        return vh;
+        // Inflate the custom layout
+        View contactView = inflater.inflate(R.layout.row_item, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(contactView);
+        return viewHolder;
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        holder.mTextView.setText(mDataSource.get(position).getEquation());
+    public void onBindViewHolder(CalculatorCustomAdapterRecycler.ViewHolder viewHolder, int position) {
+        // Get the data model based on position
+        HistoryTasks historyTasks = mDataSource.get(position);
 
+        // Set item views based on your views and data model
+        TextView textViewEquation = viewHolder.textEquation;
+        textViewEquation.setText(historyTasks.getEquation());
+
+        TextView textViewResult = viewHolder.textResult;
+        textViewResult.setText(historyTasks.getResult());
     }
 
 
