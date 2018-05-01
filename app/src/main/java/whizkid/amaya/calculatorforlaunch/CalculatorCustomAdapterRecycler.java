05845ef7cd4ecb10aa9813298eb8852a9a37cd8e
rsoni123 <Amaya123>
2018-04-30 20:54:02 -0400

    Non solid Color Operator Button

--- app/src/main/java/whizkid/amaya/calculatorforlaunch/CalculatorCustomAdapterRecycler.java
+++ app/src/main/java/whizkid/amaya/calculatorforlaunch/CalculatorCustomAdapterRecycler.java
@@ -3,9 +3,7 @@ package whizkid.amaya.calculatorforlaunch;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
